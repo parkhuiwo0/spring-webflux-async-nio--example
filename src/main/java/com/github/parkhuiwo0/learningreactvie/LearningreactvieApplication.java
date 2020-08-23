c3166cb50b1c7b4fe459840b235cbc1be0a8b1a9
HuiwooPark <parkhuiwo0@gmail.com>
2020-08-23 15:09:48 +0900

    Project Initialize

---
+++ src/main/java/com/github/parkhuiwo0/learningreactvie/LearningreactvieApplication.java
@@ -0,0 +1,13 @@
+package com.github.parkhuiwo0.learningreactvie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningreactvieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningreactvieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/parkhuiwo0/learningreactvie/LearningreactvieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.parkhuiwo0.learningreactvie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningreactvieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
