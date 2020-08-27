6ef0b25eadbc0b57943f54a6e54ac1234ce1403b
HuiwooPark <parkhuiwo0@gmail.com>
2020-08-27 10:34:49 +0900

    Application 13~16L 제거

--- src/main/java/com/github/parkhuiwo0/learningreactvie/LearningreactvieApplication.java
+++ src/main/java/com/github/parkhuiwo0/learningreactvie/LearningreactvieApplication.java
@@ -10,10 +10,6 @@ public class LearningreactvieApplication {
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(LearningreactvieApplication.class, args);
-		EchoServer echoServer = new EchoServer(123);
-		EchoClient echoClient = new EchoClient("127.0.0.1", 123);
-		echoServer.start();
-		echoClient.start();
 	}
 
 
