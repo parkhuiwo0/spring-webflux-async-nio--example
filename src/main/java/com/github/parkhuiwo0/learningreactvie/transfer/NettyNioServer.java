9c5b56443d3b273f31a9dfa2d120fdfae790e2be
HuiwooPark <parkhuiwo0@gmail.com>
2020-08-27 13:43:30 +0900

    Non-Blocking Networking with Netty (NettyNioServer.class 작성)

--- src/main/java/com/github/parkhuiwo0/learningreactvie/transfer/NettyNioServer.java
+++ src/main/java/com/github/parkhuiwo0/learningreactvie/transfer/NettyNioServer.java
@@ -1,5 +1,16 @@
 package com.github.parkhuiwo0.learningreactvie.transfer;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+
 /**
  * Created by Huiwoo Park
  * User: common
@@ -7,4 +18,32 @@ package com.github.parkhuiwo0.learningreactvie.transfer;
  * Time: 12:28 오후
  */
 public class NettyNioServer {
+    public void server(int port) throws Exception {
+        final ByteBuf buf = Unpooled.copiedBuffer("Hi\r\n", Charset.forName("UTF-8"));
+
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(group).channel(NioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress(port))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(
+                                    new ChannelInboundHandlerAdapter() {
+                                        @Override
+                                        public void channelActive(
+                                                ChannelHandlerContext ctx) throws Exception {
+                                            ctx.writeAndFlush(buf.duplicate())
+                                                    .addListener(ChannelFutureListener.CLOSE);
+                                        }
+                                    });
+                                }
+                            });
+            ChannelFuture f = b.bind().sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+    }
 }
