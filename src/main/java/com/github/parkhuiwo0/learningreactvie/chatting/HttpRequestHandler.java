64f7264c907b9d464bafe33ae3d831bfe85a013c
HuiwooPark <parkhuiwo0@gmail.com>
2020-09-06 13:38:50 +0900

    HttpRequest handler 추가

---
+++ src/main/java/com/github/parkhuiwo0/learningreactvie/chatting/HttpRequestHandler.java
@@ -0,0 +1,65 @@
+package com.github.parkhuiwo0.learningreactvie.chatting;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.*;
+
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class HttpRequestHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
+
+    private final String wsUri;
+    private static final File INDEX;
+
+    static {
+        URL location = HttpRequestHandler.class
+                .getProtectionDomain()
+                .getCodeSource().getLocation();
+
+        try {
+            String path = location.toURI() + "index.html";
+            path = !path.contains("file:") ? path : path.substring(5);
+            INDEX = new File(path);
+        } catch (URISyntaxException e) {
+            throw new IllegalStateException("Unable to locate index.html", e);
+        }
+    }
+
+    public HttpRequestHandler(String wsUri) {
+        this.wsUri = wsUri;
+    }
+
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, FullHttpRequest request) throws Exception {
+        if (wsUri.equalsIgnoreCase(request.uri())) {
+            ctx.fireChannelRead(request.retain());
+        } else {
+            if (HttpUtil.is100ContinueExpected(request)) {
+                send100Continue(ctx);
+            }
+            RandomAccessFile file = new RandomAccessFile(INDEX, "r");
+            HttpResponse response = new DefaultHttpResponse(
+                    request.protocolVersion(), HttpResponseStatus.OK);
+            response.headers().set(
+                    HttpHeaders.Names
+            )
+            )
+        }
+    }
+
+    private static void send100Continue(ChannelHandlerContext ctx) {
+        FullHttpResponse response = new DefaultFullHttpResponse(
+                HttpVersion.HTTP_1_1, HttpResponseStatus.CONTINUE);
+        ctx.writeAndFlush(response);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
